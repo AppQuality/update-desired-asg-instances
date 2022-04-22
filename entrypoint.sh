6d5cac0c15281b430995678080121231d985922a
Davide Bizzi <davide.bizzi@app-quality.com>
2022-04-22 12:34:36 +0200

    feat: Output original status

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,27 +2,6 @@
 
 set -e
 
-# if [ -z "$S3_BUCKET" ]; then
-#   echo "S3_BUCKET is not set. Quitting."
-#   exit 1
-# fi
-# if [ -z "$AWS_ACCESS_KEY_ID" ]; then
-#   echo "AWS_ACCESS_KEY_ID is not set. Quitting."
-#   exit 1
-# fi
-# if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
-#   echo "AWS_SECRET_ACCESS_KEY is not set. Quitting."
-#   exit 1
-# fi
-
-# if [-z "$FILE"]; then
-#   echo "FILE is not set. Quitting"
-#   exit 1
-# fi
-
-# if [ -z "$AWS_REGION"]; then
-#   AWS_REGION="us-east-1"
-# fi
 
 mkdir -p ~/.aws
 
@@ -32,12 +11,22 @@ echo "[default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
-aws autoscaling   update-auto-scaling-group  --auto-scaling-group-name $ASG_NAME --desired-capacity $CAPACITY --max-size $CAPACITY
 
-CURRENT_CAPACITY=0
+CURRENT_STATUS=$(aws autoscaling   describe-auto-scaling-groups  --auto-scaling-group-name $ASG_NAME | jq -r  '.AutoScalingGroups[0]')
+ORIGINAL_MAX=$(echo $CURRENT_STATUS | jq -r  '.MaxSize')
+ORIGINAL_MIN=$(echo $CURRENT_STATUS | jq -r  '.MinSize')
+ORIGINAL_CAPACITY=$(echo $CURRENT_STATUS | jq -r  '.DesiredCapacity')
+
+
+aws autoscaling   update-auto-scaling-group  --auto-scaling-group-name $ASG_NAME --desired-capacity $CAPACITY --max-size $CAPACITY
+CURRENT_CAPACITY="$ORIGINAL_CAPACITY"
 while [ "$CURRENT_CAPACITY" != "$CAPACITY" ]; do
     CURRENT_CAPACITY=$(aws autoscaling   describe-auto-scaling-groups  --auto-scaling-group-name $ASG_NAME | jq  '.AutoScalingGroups[0] | [.Instances[] | select(.HealthStatus == "Healthy")] | length')
     echo "Current capacity: $CURRENT_CAPACITY"
     sleep 5
 done
 rm -rf ~/.aws
+
+echo "::set-output name=original_max::$ORIGINAL_MAX"
+echo "::set-output name=original_min::$ORIGINAL_MIN"
+echo "::set-output name=original_desired::$ORIGINAL_DESIRED"
